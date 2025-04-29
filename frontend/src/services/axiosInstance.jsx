df5994bade3c690e388d84baa459d8998c3b166c
yashvi03 <yashviagrawal03@gmail.com>
2025-04-29 10:59:13 +0530

    Update axiosInstance.jsx

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL:'https://custom-website-builder.onrender.com',
   headers: {
     "Content-Type": "application/json",
   },
