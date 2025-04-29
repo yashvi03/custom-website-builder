7a58cd1eb675124cd4e826240b43a399b467c4bf
yashvi03 <yashviagrawal03@gmail.com>
2025-04-29 12:13:03 +0530

    updated homepage url

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:8000/",
-  baseURL:'https://custom-website-builder.onrender.com',
+  baseURL:'https://custom-website-builder.onrender.com/',
   headers: {
     "Content-Type": "application/json",
   },
